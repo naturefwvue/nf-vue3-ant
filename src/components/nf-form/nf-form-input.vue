68909af71b59adf8a50fb3f80f9f0f088abed24d
jyk0011 <jyk0013@163.com>
2020-09-17 17:38:36 +0800

    修改input

--- src/components/nf-form/nf-form-input.vue
+++ src/components/nf-form/nf-form-input.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="components-input-demo-presuffix">
     <a-input
-      v-model:value="value"
-      size="small"
-      :type="type[meta.controlType]"
+      :id="'id' + meta.controlId"
       :name="'c' + meta.controlId"
       :value="modelValue"
+      :autofocus="meta.autofocus"
       :disabled="meta.disabled"
       :readonly="meta.readonly"
-      :class="meta.class"
       :placeholder="meta.placeholder"
       :title="meta.title"
       :maxlength="meta.maxlength"
       :autocomplete="meta.autocomplete"
       :key="'ckey_'+meta.controlId"
+      size="small"
+      @input="myInput"
       >
     </a-input>
   </div>
@@ -46,81 +46,34 @@ export default {
             type: Boolean,
             default: false
           },
+          required: { // 必填
+            type: Boolean,
+            default: true
+          },
           disabled: {
             // 是否禁用
             type: Boolean,
             default: false
-          },
-          required: { // 必填
-            type: Boolean,
-            default: true
           },
           readonly: { // 只读
             type: Boolean,
             default: false
           },
           pattern: String, // 用正则做验证。
-          class: String, // 'cssTxt input_t1'
           placeholder: String,
           title: String, // 提示信息
-          size: Number, // 字符宽度
           maxlength: Number, // 最大字符数
           autocomplete: { // off
             type: String,
             default: 'on'
-          },
-          optionKey: String, // 备选文字标识
-          optionItem: Object // 备选的选项
+          }
         }
       }
     }
   },
-  data: () => {
-    return {
-      value: '',
-      type: {
-        100: 'textarea', // 多行文本框
-        101: 'text', // 单行文本框
-        102: 'password', // 密码
-        103: 'tel', // 电话
-        104: 'email', // 电子邮件
-        105: 'url', // url
-        106: 'search', // 搜索
-        107: 'text', // 当前登录人
-        120: 'fulltext', // 富文本编辑器
-        131: 'number', // 数字
-        132: 'range', // 滑块
-        140: 'date', // 日期
-        141: 'datetime-local', // 日期时间
-        142: 'time', // 时间
-        143: 'month', // 年月
-        144: 'week', // 年周
-        150: 'file', // 上传文件
-        151: 'file', // 上传图片
-        160: 'color', // 颜色
-        170: 'text', // 弹窗选择记录
-        180: 'checkbox', // 勾选
-        182: 'checkboxs', // 多选组
-        183: 'radios', // 单选组
-        190: 'select', // 下拉列表框 单选
-        191: 'selects', // 列表框 多选
-        192: 'selectMore' // 联动下拉列表框
-      }
-    }
-  },
-  watch: {
-    value: function (val) {
-      var colName = this.meta.colName
-      this.$emit('update:modelValue', val) // 返回给调用者
-      this.$emit('getvalue', val, colName) // 返回给中间组件
-    }
-  },
-  created: function () {
-    this.value = this.modelValue
-  },
   methods: {
     myInput: function (e) {
-      var returnValue = this.value1
+      var returnValue = e.target.value
       var colName = this.meta.colName // event.target.getAttribute('colname')
       this.$emit('update:modelValue', returnValue) // 返回给调用者
       this.$emit('getvalue', returnValue, colName) // 返回给中间组件
